d049e78d213b4f2db459415b22698f6268bcb6fd
ᗪEᑎIᗪ <denisdubov88@gmail.com>
2026-09-14 10:39:02 +0300

    [Creational] - Prototype pattern example

--- gof/creationals/prototype.dart
+++ gof/creationals/prototype.dart
@@ -1,12 +1,59 @@
 void main() {
-  final Car car = Car(CarType.passenger);
-  print(car);
-  final Car lorry = car.cloneLorry();
-  print(lorry);
-  final Car minivan = car.cloneMinivan();
-  print(minivan);
+  // final Car car = Car(CarType.passenger);
+  // print(car);
+  // final Car lorry = car.cloneLorry();
+  // print(lorry);
+  // final Car minivan = car.cloneMinivan();
+  // print(minivan);
+
+  final fastGhosts = Spawner(Ghost(15, 10));
+  final slowGhosts = Spawner(Ghost(40, 2));
+  final demons = Spawner(Demon(7));
+
+  print(fastGhosts.spawn()); // Ghost(hp: 15, speed: 10)
+  print(slowGhosts.spawn()); // Ghost(hp: 40, speed: 2)
+  print(demons.spawn());     // Demon(rage: 7)
 }
 
+abstract class Monster {
+  Monster clone();
+}
+
+class Ghost extends Monster {
+  Ghost(this.health, this.speed);
+
+  final int health;
+  final int speed;
+
+  // Covariant return type: Dart дозволяє звузити тип у override
+  @override
+  Ghost clone() => Ghost(health, speed);
+
+  @override
+  String toString() => 'Ghost(hp: $health, speed: $speed)';
+}
+
+class Demon extends Monster {
+  Demon(this.rage);
+  final int rage;
+
+  @override
+  Demon clone() => Demon(rage);
+
+  @override
+  String toString() => 'Demon(rage: $rage)';
+}
+
+/// Один спавнер на всі види монстрів
+class Spawner {
+  Spawner(this._prototype);
+  final Monster _prototype;
+
+  Monster spawn() => _prototype.clone();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 abstract interface class ICarPrototype {
   Car clonePassenger();
   Car cloneLorry();
